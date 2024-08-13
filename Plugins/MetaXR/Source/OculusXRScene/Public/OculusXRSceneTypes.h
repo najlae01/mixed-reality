b0579fc6870dea39f3d975d81a492e5037c3526f
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-08-13 02:15:48 +0100

    first commit

---
+++ Plugins/MetaXR/Source/OculusXRScene/Public/OculusXRSceneTypes.h
@@ -0,0 +1,14 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#pragma once
+
+#include "OculusXRSceneTypes.generated.h"
+
+USTRUCT()
+struct OCULUSXRSCENE_API FSceneTypesPlaceholder
+{
+	GENERATED_BODY()
+public:
+};
+
+
